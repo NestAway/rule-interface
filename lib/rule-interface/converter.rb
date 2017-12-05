1fb86815df2aeb28cbf9e3b3da564b39217a5f1e
Vishal Vijay <0vishalvijay0@gmail.com>
2017-12-05 13:13:24 +0530

    Fix executer

--- lib/rule-interface/converter.rb
+++ lib/rule-interface/converter.rb
@@ -10,7 +10,6 @@ module RuleInterface
       #insert namespace object
       payload[:commands] << insert_object(
         data_object: {
-          id: namespace,
           name: namespace
         },
         object_type: 'namespace',
@@ -46,7 +45,9 @@ module RuleInterface
     private
 
     def insert_object(data_object:, object_type:, package:, return_object: true)
-      raise Error::CommonError, "Object must have an id attribute: #{data_object.to_json}" if data_object[:id].blank?
+      if return_object && data_object[:id].blank?
+        raise Error::CommonError, "Object must have an id attribute: #{data_object.to_json}"
+      end
 
       data_object = data_object.inject({}) do |m, (k,v)|
         m[k.to_s.camelize(:lower).to_sym] = v; m
@@ -56,7 +57,7 @@ module RuleInterface
       insert_object = {}
       insert_object[:insert] = {}
       insert_object[:insert]['return-object'] = return_object
-      insert_object[:insert]['out-identifier'] = "#{object_type}_#{data_object[:id]}" if return_object
+      insert_object[:insert]['out-identifier'] = "#{object_type}##{data_object[:id]}" if return_object
 
       object = {}
       object_name = "#{package}.#{object_type}"
